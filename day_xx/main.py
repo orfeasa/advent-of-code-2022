8faa432fd0c9d902088f3feea565fa51d2fb69d9
Orfeas Antoniou <orfeas@orfeasa.com>
2022-12-01 09:55:51 +0000

    Solve day 01 and small edits

---
+++ day_xx/main.py
@@ -0,0 +1,15 @@
+def part_one(filename: str) -> int:
+    return 0
+
+
+def part_two(filename: str) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    input_path = "./day_xx/input.txt"
+    print("---Part One---")
+    print(part_one(input_path))
+
+    print("---Part Two---")
+    print(part_two(input_path))
