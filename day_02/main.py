2544531196c6b1daa90c11515831bab32efd80cc
Orfeas Antoniou <orfeas@orfeasa.com>
2022-12-02 11:21:32 +0000

    Improve win/lose calculations

--- day_02/main.py
+++ day_02/main.py
@@ -6,18 +6,46 @@ class Move(Enum):
     PAPER = 2
     SCISSORS = 3
 
+    def beats(self):
+        match self:
+            case Move.ROCK:
+                return Move.SCISSORS
+            case Move.PAPER:
+                return Move.ROCK
+            case Move.SCISSORS:
+                return Move.PAPER
 
-def to_move(move: str) -> Move:
-    match move:
-        case "A" | "X":
-            return Move.ROCK
-        case "B" | "Y":
-            return Move.PAPER
-        case "C" | "Z":
-            return Move.SCISSORS
+    def loses_by(self):
+        match self:
+            case Move.ROCK:
+                return Move.PAPER
+            case Move.PAPER:
+                return Move.SCISSORS
+            case Move.SCISSORS:
+                return Move.ROCK
+
+
+    @classmethod
+    def to_move(cls, move: str):
+        match move:
+            case "A" | "X":
+                return cls.ROCK
+            case "B" | "Y":
+                return cls.PAPER
+            case "C" | "Z":
+                return cls.SCISSORS
+
+
+def calc_outcome_score(opp_move, your_move: Move) -> int:
+    match (opp_move, your_move):
+        case (move1, move2) if move1 == move2:
+            return 3
+        case (move1, move2) if move2 == move1.beats():
+            return 0
+        case (move1, move2) if move2 == move1.loses_by():
+            return 6
         case _:
-            raise ValueError(f"Move {move} not recognised")
-
+            raise ValueError("Invalid moves")
 
 def part_one(filename: str) -> int:
     with open(filename) as f:
@@ -31,27 +59,10 @@ def part_one(filename: str) -> int:
     total_score = 0
     for move in moves:
         total_score += move_to_score[move[1]]
-        total_score += calc_outcome_score(to_move(move[0]), to_move(move[1]))
+        total_score += calc_outcome_score(Move.to_move(move[0]), Move.to_move(move[1]))
 
     return total_score
 
-
-def calc_outcome_score(opp_move, your_move: Move) -> int:
-    match (opp_move, your_move):
-        case (move1, move2) if move1 == move2:
-            return 3
-        case (Move.ROCK, Move.SCISSORS) | (Move.PAPER, Move.ROCK) | (
-            Move.SCISSORS,
-            Move.PAPER,
-        ):
-            return 0
-        case (Move.SCISSORS, Move.ROCK) | (Move.ROCK, Move.PAPER) | (
-            Move.PAPER,
-            Move.SCISSORS,
-        ):
-            return 6
-        case _:
-            raise ValueError("Invalid moves")
 
 
 def part_two(filename: str) -> int:
