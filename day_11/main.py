d09db635839f574386015170c81017ded300a6c8
Orfeas Antoniou <orfeas@orfeasa.com>
2022-12-11 22:13:49 +0000

    WIP

--- day_11/main.py
+++ day_11/main.py
@@ -4,34 +4,42 @@ from typing import Callable
 class Monkey:
     id: int
     items: list[int]
-    operation: Callable[[int], int]
-    test: Callable[[int], bool]
+    operation: Callable[[int, int], int]
+    test_with: int
     if_true: int
     if_false: int
     count_inspections: int
+    mul_with_self: bool = False
+    mul_with_val: int = 0
 
     def __init__(self, lines_str: str):
         lines = [line.strip() for line in lines_str.split("\n")]
         self.id = int(lines[0].split(" ")[1].strip(":"))
         self.items = [int(item) for item in lines[1].split(": ")[1].split(", ")]
-        self.operation = self.eval_operation(lines[2].split(" = ")[-1])
-        self.test = lambda x: x % int(lines[3].split(": ")[1].split("by ")[1]) == 0
+        test_with = int(lines[3].lstrip("Test: divisible by "))
+        self.operation = self.eval_operation(lines[2].split(" = ")[-1], test_with)
         self.if_true = int(lines[4].split("monkey ")[1])
         self.if_false = int(lines[5].split("monkey ")[1])
         self.count_inspections = 0
 
-    @staticmethod
-    def eval_operation(operation: str) -> Callable[[int], int]:
+    def eval_operation(self, operation: str, mod: int) -> Callable[[int, int], int]:
         left, operand, right = operation.split(" ")
         match [left, operand, right]:
             case ["old", "+", "old"]:
-                return lambda old: old + old
+                return lambda old, divider: ((old + old) // divider) % mod
             case ["old", "*", "old"]:
-                return lambda old: old * old
+                self.mul_with_self = True
+                return (
+                    lambda old, divider: (((old % mod) * (old % mod)) // divider) % mod
+                )
             case ["old", "+", _ as num]:
-                return lambda old: old + int(num)
+                return lambda old, divider: ((old + int(num)) // divider) % mod
             case ["old", "*", _ as num]:
-                return lambda old: old * int(num)
+                return (
+                    lambda old, divider: ((int(num) % mod) * (old % mod) // divider)
+                    % mod
+                )
+        raise ValueError(f"Unrecognised operation: {operation}")
 
     def __repr__(self) -> str:
         return f"Monkey {self.id}: {', '.join(str(item) for item in self.items)}"
@@ -48,7 +56,7 @@ def part_one(filename: str) -> int:
 def part_two(filename: str) -> int:
     monkeys = parse_input(filename)
     for _ in range(10000):
-        run_round(monkeys)
+        run_round(monkeys, 1)
         if _ + 1 in [
             1,
             20,
@@ -73,15 +81,13 @@ def part_two(filename: str) -> int:
 
 
 def run_round(monkeys: dict[int, "Monkey"], worry_divider=1) -> None:
-    for id in range(len(monkeys)):
+    for id, _ in enumerate((monkeys)):
         monkey = monkeys[id]
         if len(monkey.items) == 0:
             continue
         monkey.count_inspections += len(monkey.items)
         for item in monkey.items:
-            item = monkey.operation(item)
-            item //= worry_divider
-            if monkey.test(item):
+            if monkey.operation(item, worry_divider) == 0:
                 monkeys[monkey.if_true].items.append(item)
             else:
                 monkeys[monkey.if_false].items.append(item)
