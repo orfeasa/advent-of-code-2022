c54888f624736e519130c560701dc710cf552413
Orfeas Antoniou <orfeas@orfeasa.com>
2022-12-12 15:27:03 +0000

    Make cleaner

--- day_12/main.py
+++ day_12/main.py
@@ -1,15 +1,39 @@
-from dataclasses import dataclass
+def part_one(filename: str) -> int:
+    lines = parse_input(filename)
+    start, end = get_start_end(lines)
+    graph = construct_graph(lines)
+    visited = bfs(graph, start)
+    return visited[end]
 
 
-@dataclass
-class Node:
-    coord: tuple[int, int]
-    children: set[tuple[int, int]]
-    elevation: int
+def part_two(filename: str) -> int:
+    lines = parse_input(filename)
+    start, end = get_start_end(lines)
+    graph = construct_graph(lines)
+    start_candidates = {(x, y)
+                        for x, y in graph if get_elevation(lines[y][x]) == 0}
+    steps_required = []
+    for start in start_candidates:
+        visited = bfs(graph, start)
+        if end in visited:
+            steps_required.append(visited[end])
+    return min(steps_required)
 
-    @staticmethod
-    def __repr__(self):
-        return f"Node({self.coord}, {self.children})"
+
+def parse_input(filename: str) -> list[str]:
+    with open(filename, encoding="utf8") as f:
+        lines = [line.strip() for line in f.readlines()]
+    return lines
+
+
+def get_start_end(lines: list[str]) -> tuple[tuple[int, int], tuple[int, int]]:
+    start = end = (-1, -1)
+    for y, line in enumerate(lines):
+        if line.find("S") != -1:
+            start = (line.find("S"), y)
+        if line.find("E") != -1:
+            end = (line.find("E"), y)
+    return start, end
 
 
 def get_elevation(elevation: str) -> int:
@@ -20,22 +44,7 @@ def get_elevation(elevation: str) -> int:
     return ord(elevation) - ord("a")
 
 
-def part_one(filename: str) -> int:
-    start, end, graph = parse_input(filename)
-    visited = bfs(graph, start)
-    return visited[end]
-
-
-def part_two(filename: str) -> int:
-    with open(filename, encoding="utf8") as f:
-        lines = [line.strip() for line in f.readlines()]
-    start = end = (-1, -1)
-    for y, line in enumerate(lines):
-        if line.find("S") != -1:
-            start = (line.find("S"), y)
-        if line.find("E") != -1:
-            end = (line.find("E"), y)
-    start_candidates = {start}
+def construct_graph(lines: list[str]) -> dict[tuple[int, int], set[tuple[int, int]]]:
     graph = {}
     for y, line in enumerate(lines):
         for x, char in enumerate(line):
@@ -50,42 +59,7 @@ def part_two(filename: str) -> int:
                 if (get_elevation(lines[y1][x1])-get_elevation(char) <= 1):
                     children.add((x1, y1))
             graph[coord] = children
-            if char == "a":
-                start_candidates.add(coord)
-
-    steps_required = []
-    for start in start_candidates:
-        visited = bfs(graph, start)
-        if end in visited:
-            steps_required.append(visited[end])
-    return min(steps_required)
-
-
-def parse_input(filename: str) -> tuple[tuple[int, int], tuple[int, int], dict[tuple[int, int], set[tuple[int, int]]]]:
-    with open(filename, encoding="utf8") as f:
-        lines = [line.strip() for line in f.readlines()]
-    start = end = (-1, -1)
-    for y, line in enumerate(lines):
-        if line.find("S") != -1:
-            start = (line.find("S"), y)
-        if line.find("E") != -1:
-            end = (line.find("E"), y)
-
-    graph = {}
-    for y, line in enumerate(lines):
-        for x, char in enumerate(line):
-            coord = (x, y)
-            children = set()
-            candidates = [
-                (x + dx, y + dy)
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]
-                if 0 <= x + dx < len(line) and 0 <= y + dy < len(lines)
-            ]
-            for x1, y1 in candidates:
-                if (get_elevation(lines[y1][x1])-get_elevation(char) <= 1):
-                    children.add((x1, y1))
-            graph[coord] = children
-    return start, end, graph
+    return graph
 
 
 def bfs(
