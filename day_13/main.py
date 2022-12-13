01ff4146af372f44e2a2ad4b417fd9569252fbae
Orfeas Antoniou <orfeas@orfeasa.com>
2022-12-13 13:41:39 +0000

    Make day 13 code cleaner

--- day_13/main.py
+++ day_13/main.py
@@ -5,17 +5,16 @@ from functools import cmp_to_key, reduce
 
 def part_one(filename: str) -> int:
     lines = parse_input(filename)
-    return sum([i + 1 for i in range(len(lines)) if cmp_values(*lines[i]) == 1])
+    return sum([ind for ind, line in enumerate(lines, 1) if cmp_values(*line) == 1])
 
 
 def part_two(filename: str) -> int:
     lines = parse_input(filename)
     keys = [[[2]], [[6]]]
-    flat_lines = [item for sublist in lines for item in sublist]
-    flat_lines.extend(keys)
+    flat_lines = keys + [item for sublist in lines for item in sublist]
     flat_lines.sort(key=cmp_to_key(cmp_values), reverse=True)
     return reduce(
-        operator.mul, [ind + 1 for ind, x in enumerate(flat_lines) if x in keys]
+        operator.mul, [ind for ind, x in enumerate(flat_lines, 1) if x in keys]
     )
 
 
@@ -29,24 +28,18 @@ def parse_input(filename: str) -> list:
 
 # returns 1 if values are in the right order, -1 if not, 0 if equal
 def cmp_values(left: int | list, right: int | list) -> int:
-    def cmp(left, right: int) -> int:
-        return (left < right) - (left > right)
-
-    if isinstance(left, int) and isinstance(right, int):
-        if left == right:
-            return 0
-        return cmp(left, right)
-    if isinstance(left, list) and isinstance(right, list):
-        for i in range(min(len(left), len(right))):
-            if (cmp_val := cmp_values(left[i], right[i])) != 0:
-                return cmp_val
-        if len(left) == len(right):
-            return 0
-        return cmp(len(left), len(right))
-    if isinstance(left, int) and isinstance(right, list):
-        return cmp_values([left], right)
-    if isinstance(left, list) and isinstance(right, int):
-        return cmp_values(left, [right])
+    match left, right:
+        case int(), int():
+            return (left < right) - (left > right)
+        case list(), list():
+            for cmp_val in map(cmp_values, left, right):
+                if cmp_val:
+                    return cmp_val
+            return cmp_values(len(left), len(right))
+        case int(), list():
+            return cmp_values([left], right)
+        case list(), int():
+            return cmp_values(left, [right])
     raise ValueError(f"Invalid types: {type(left)} and {type(right)}")
 
 
