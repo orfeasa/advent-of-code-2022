af96c6700b2207d7f6cec1cc3555eb1de1ebb9ae
Orfeas Antoniou <orfeas@orfeasa.com>
2022-12-22 09:49:15 +0000

    Input

---
+++ day_22/main.py
@@ -0,0 +1,20 @@
+def part_one(filename: str) -> int:
+    return 0
+
+
+def part_two(filename: str) -> int:
+    return 0
+
+
+def parse_input(filename: str):
+    with open(filename, encoding="utf8") as f:
+        line.strip().split("\n\n")
+
+
+if __name__ == "__main__":
+    input_path = "./day_22/input.txt"
+    print("---Part One---")
+    print(part_one(input_path))
+
+    print("---Part Two---")
+    print(part_two(input_path))
